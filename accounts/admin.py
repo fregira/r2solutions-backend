ba667200f0ddbd247588f1cdad059059975e985f
fregira <reliable.resource.02@gmail.com>
2025-06-09 07:27:59 +0800

    Add CustomUser admin registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'phone', 'service_interest', 'member_since', 'is_active']
+    list_filter = ['service_interest', 'is_active', 'member_since']
+    search_fields = ['username', 'email', 'first_name', 'last_name', 'phone']
+    readonly_fields = ['member_since']
